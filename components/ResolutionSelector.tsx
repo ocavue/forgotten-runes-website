e7d3ed05142ba1a8971cfebe8188c1c6dc60b7fc
Forgotten <forgottenrunes@protonmail.com>
2022-02-08 12:25:55 -0600

    resolution and wizard selection

--- components/ResolutionSelector.tsx
+++ components/ResolutionSelector.tsx
@@ -4,6 +4,8 @@ import styled from "@emotion/styled";
 import Select from "react-select";
 import { DEVICE_ASPECT_RATIOS } from "../lib/util/devices";
 import { customSelectStyles } from "./TokenSelector";
+import { getSelectorsByUserAgent } from "react-device-detect";
+import { useWindowSize } from "@react-hook/window-size";
 
 const resolutionOptions = DEVICE_ASPECT_RATIOS.map((w) => {
   const name = `${w.name} (${w.width * w.ratio}x${w.height * w.ratio})`;
@@ -17,9 +19,42 @@ type Props = {
 const ResolutionSelectorElement = styled.div``;
 
 export default function ResolutionSelector({ onChange }: Props) {
-  const [resolutionOption, setResolutionOption] = useState(
-    resolutionOptions.find((r: any) => r.label.match(/iPhone 8/))
+  const selectors = getSelectorsByUserAgent(
+    typeof window !== "undefined" ? window?.navigator?.userAgent : ""
   );
+  const [width, height] = useWindowSize();
+  const { isMobile, mobileVendor, mobileModel } = selectors
+    ? selectors
+    : { isMobile: false, mobileVendor: "", mobileModel: "" };
+
+  let defaultResolution = resolutionOptions.find((r: any) =>
+    r.label.match(/iPhone 8/)
+  );
+
+  if (isMobile) {
+    const matchingDevice = resolutionOptions.find((r: any) => {
+      return r.value.width === width && r.value.height === height;
+    });
+    if (matchingDevice) {
+      defaultResolution = matchingDevice;
+    } else {
+      const deviceName = `${mobileVendor} ${mobileModel}`;
+      defaultResolution = {
+        label: deviceName,
+        value: {
+          name: deviceName,
+          width,
+          height,
+          ratio: typeof window !== "undefined" ? window.devicePixelRatio : 1,
+          type: "mobile",
+        },
+      };
+      console.log("defaultResolution: ", defaultResolution);
+    }
+  }
+
+  const [resolutionOption, setResolutionOption] = useState(defaultResolution);
+  // console.log("resolutionOption: ", resolutionOption);
 
   useEffect(() => {
     onChange({ resolutionOption });
