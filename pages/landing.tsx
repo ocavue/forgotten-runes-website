50892ce3bdbcb94598f147e6f767fb81c9247405
wagmiwiz <wagmiwiz@users.noreply.github.com>
2022-02-18 10:31:03 +0000

    Contenful PoC

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -2,19 +2,41 @@ import { GetStaticProps } from "next";
 import InfoPageLayout from "../components/InfoPageLayout";
 import { createClient, Entry } from "contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
+import { Flex } from "rebass";
 
 export default function WtfPage({ entry }: { entry: Entry<any> }) {
   return (
     <InfoPageLayout>
-      {/*{documentToReactComponents(entry.fields.mainContent)}*/}
+      <Flex flexDirection={"column"} p={2}>
+        {documentToReactComponents(entry.fields.mainContent, {
+          renderNode: {
+            [BLOCKS.EMBEDDED_ASSET]: (node) => (
+              <img
+                src={node.data?.target?.fields?.file?.url}
+                alt={node.data?.target?.fields?.title}
+                style={{ objectFit: "cover" }}
+              />
+            ),
+          },
+        })}
+      </Flex>
     </InfoPageLayout>
   );
 }
 
 export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE as string,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN as string,
+  });
+
+  const entry = await client.getEntry("6N3xs2AZQ1llxbyf5BFJz6"); // TODO: maybe some json config for specific static pages
+  // console.log(entry);
+
   return {
     props: {
-      entry: {},
+      entry: entry,
     },
   };
 };
