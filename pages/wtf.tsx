ee0a0ccd6e871ff232ac42e479e2ef64594a0074
wagmiwiz <wagmiwiz@users.noreply.github.com>
2022-03-04 11:26:40 +0000

    FAQ page updates

--- pages/wtf.tsx
+++ pages/wtf.tsx
@@ -8,81 +8,100 @@ import { Box, Flex } from "rebass";
 export default function FaqPage({ entry }: { entry: Entry<any> }) {
   return (
     <InfoPageLayout size={"wide"}>
-      <Flex flexDirection={"column"} width={"100%"}>
+      <Flex flexDirection={"column"} width={"100%"} alignItems={"center"}>
         {entry.fields.topBanner && (
           <img
             src={entry.fields.topBanner.fields.file.url}
             alt={entry.fields.topBanner.fields.title}
           />
         )}
-      </Flex>
-      <Flex flexDirection={"column"} px={[0, 6]}>
-        {entry.fields.faqEntries.map((faqEntry: any) => {
-          const columnImage = faqEntry.fields.inlineImage ? (
-            <img
-              style={{
-                objectFit: "contain",
-                flex: "0 0 25%",
-                width: "100%",
-                height: "auto",
-              }}
-              src={faqEntry.fields.inlineImage.fields.file.url}
-              alt={faqEntry.fields.inlineImage.fields.title}
-            />
-          ) : null;
+        <Flex flexDirection={"column"} px={[1, 2]} maxWidth={"860px"}>
+          {entry.fields.faqEntries.map((faqEntry: any) => {
+            const columnImage = faqEntry.fields.inlineImage ? (
+              <img
+                style={{
+                  objectFit: "contain",
+                  flex: "0 0 25%",
+                  width: "100%",
+                  height: "auto",
+                }}
+                src={faqEntry.fields.inlineImage.fields.file.url}
+                alt={faqEntry.fields.inlineImage.fields.title}
+              />
+            ) : null;
 
-          return (
-            <Box pt={4} key={faqEntry.sys.id}>
-              <Flex
-                flexDirection={"row"}
-                alignItems={"flex-start"}
-                justifyContent={"space-between"}
-              >
-                {columnImage &&
-                (!faqEntry.fields.inlineImageAlignment ||
-                  faqEntry.fields.inlineImageAlignment === "left") ? (
-                  <Box pr={3}>{columnImage}</Box>
-                ) : null}
-                <Flex flexDirection={"column"} flex={"1 0 75%"}>
-                  <h2 style={{ margin: 0 }}>{faqEntry.fields.title}</h2>
-                  {documentToReactComponents(faqEntry.fields.body, {
-                    renderText: (text: string) => {
-                      return text
-                        .split("\n")
-                        .reduce(
-                          (
-                            children: any[],
-                            textSegment: string,
-                            index: number
-                          ) => {
-                            return [
-                              ...children,
-                              index > 0 && <br key={index} />,
-                              textSegment,
-                            ];
-                          },
-                          []
-                        );
-                    },
-                    renderNode: {
-                      [BLOCKS.EMBEDDED_ASSET]: (node) => (
-                        <img
-                          src={node.data?.target?.fields?.file?.url}
-                          alt={node.data?.target?.fields?.title}
-                          style={{ objectFit: "cover", alignSelf: "center" }}
-                        />
-                      ),
-                    },
-                  })}
-                </Flex>
-                {columnImage &&
-                faqEntry.fields?.inlineImageAlignment === "right" ? (
-                  <Box pl={3}>{columnImage}</Box>
-                ) : null}
-              </Flex>
-            </Box>
-          );
-        })}
+            const columnImageWidthPercent =
+              faqEntry.fields.inlineImageWidth ?? 25;
+
+            const isColumnImageLeftAligned =
+              !faqEntry.fields.inlineImageAlignment ||
+              faqEntry.fields.inlineImageAlignment === "left";
+
+            const columns = columnImage
+              ? isColumnImageLeftAligned
+                ? `${columnImageWidthPercent}% ${
+                    100 - columnImageWidthPercent
+                  }%`
+                : `${
+                    100 - columnImageWidthPercent
+                  }% ${columnImageWidthPercent}%`
+              : "1fr";
+
+            return (
+              <Box pt={4} key={faqEntry.sys.id}>
+                <div
+                  style={{
+                    display: "grid",
+                    gridTemplateColumns: columns,
+                  }}
+                >
+                  {columnImage && isColumnImageLeftAligned ? (
+                    <Box pr={3}>{columnImage}</Box>
+                  ) : null}
+                  <Flex flexDirection={"column"} flex={"1 0 75%"}>
+                    <h2 style={{ margin: 0 }}>{faqEntry.fields.title}</h2>
+                    {documentToReactComponents(faqEntry.fields.body, {
+                      renderText: (text: string) => {
+                        return text
+                          .split("\n")
+                          .reduce(
+                            (
+                              children: any[],
+                              textSegment: string,
+                              index: number
+                            ) => {
+                              return [
+                                ...children,
+                                index > 0 && <br key={index} />,
+                                textSegment,
+                              ];
+                            },
+                            []
+                          );
+                      },
+                      renderNode: {
+                        [BLOCKS.EMBEDDED_ASSET]: (node) => (
+                          <img
+                            src={node.data?.target?.fields?.file?.url}
+                            alt={node.data?.target?.fields?.title}
+                            style={{
+                              objectFit: "cover",
+                              alignSelf: "center",
+                              maxWidth: "100%",
+                            }}
+                          />
+                        ),
+                      },
+                    })}
+                  </Flex>
+                  {columnImage && !isColumnImageLeftAligned ? (
+                    <Box pl={3}>{columnImage}</Box>
+                  ) : null}
+                </div>
+              </Box>
+            );
+          })}
+        </Flex>
       </Flex>
     </InfoPageLayout>
   );
